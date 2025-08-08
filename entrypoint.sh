092bf5aa8eb6eb3a2e76f455aa4538e79f32b421
Matheus Vasques Santos <matheusvasquessantos@gmail.com>
2025-08-08 12:13:12 -0300

    Removido_debugpy_dependencia

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,5 @@ until PGPASSWORD=$PASSWORD pg_isready -h $HOST -U $USER -p 5432; do
 done
 echo "Postgres está disponível - iniciando Odoo"
 
-# Instala o debugpy para debug
-pip install --user debugpy
-
-# Inicia o Odoo com debugpy usando o arquivo de configuração
-/usr/bin/python3 -m debugpy --listen 0.0.0.0:5678 --wait-for-client /usr/bin/odoo -c /etc/odoo/odoo.conf
+# Inicia o Odoo diretamente, sem o debugpy
+/usr/bin/odoo -c /etc/odoo/odoo.conf
